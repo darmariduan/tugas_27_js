22ac013db92448929b77762d5c450e6cf4cfb7a5
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-21 08:43:04 +0700

    Tugas 27 Js

---
+++ tugas27.js
@@ -0,0 +1,12 @@
+function tugas27() {
+var a = ['Jalan pangeran Tirtayasa No ',70];
+var b = ['No','rumah saya',30,'A'];
+
+gabung = a.join().split(',');
+gabungan = gabung.join("");
+console.log(gabungan);
+
+gabungan = b.join(' ');
+console.log(gabungan);
+}
+tugas27()
